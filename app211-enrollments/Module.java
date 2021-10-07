16fa2e636c492bd87d0ad373f2585d0c5f0d30f5
MaxMaxGoodwin <maxgoodmax@hotmail.co.uk>
2021-10-07 22:48:35 +0100

    Completed some steps for Module code

--- app211-enrollments/Module.java
+++ app211-enrollments/Module.java
@@ -1,33 +1,46 @@
 
 /**
- * Write a description of class Module here.
+ * A program that takes user input data to add a new module within a
+ * course which can then be printed out in the correct format.
  *
  * @author (Max Goodwin)
- * @version (v1.0 06/10/2021)
+ * @version (v1.0 07/10/2021)
  */
+//Defines class for Module
 public class Module
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Module
-     */
-    public Module()
+    //Assigns a variable for module code as an integer
+    private int moduleCode;
+    
+    //Assigns a variable for module title as a string
+    private String title;
+    
+    //Assigns a variable for credit value as an integer
+    private int creditValue;
+    
+    //Assigns a variable for module credit as an integer
+    private int modCredit;
+    
+    //Method which asks for user input to get values of code, title and credit value
+    public void getValues(int moduleCode, String title, int creditValue)
     {
-        // initialise instance variables
-        x = 0;
+        this.moduleCode = moduleCode;
+        this.title = title;
+        this.creditValue = creditValue;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    //Method which asks for user input to get value of module credit
+    public void setModCred(int modCredit)
     {
-        // put your code here
-        return x + y;
+        this.modCredit = modCredit;
+    }
+    
+    //Method that prints out the values of title, code, credit value and module credit
+    public void printModule()
+    {
+        System.out.println("Title: " + title);
+        System.out.println("Code: " + moduleCode);
+        System.out.println("Credit Value: " + creditValue);
+        System.out.println("Module Credit: " + modCredit);
     }
 }
